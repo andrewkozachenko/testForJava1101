ea38cbd50907a7a7e98acdf7469b82f68e8ea324
Andrew Kozachenko <andrew.kozachenko@gmail.com>
2019-02-03 22:12:14 +0200

    first commit

---
+++ src/com/kozachenko/test/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.kozachenko.test;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello boys and girls from Java 1101 ");
+    }
+
+}
